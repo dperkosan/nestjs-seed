bbbfad98cfd4f98f0497199d1654ed985f4e7e48
Damir Perkosan <dperkosan@gmail.com>
2022-11-27 15:32:11 +0100

    crud operations in controller for user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Post()
   create(@Body() userDto) {
     return userDto;
+  }
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all users. Limit ${limit}, offset: ${offset}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} user`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userDto) {
+    return `This action updates #${id} user`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} user`;
   }
 }
