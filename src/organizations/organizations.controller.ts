7dae32ea74a6bf2b8c8ebd6010e5f6ee97d3fde6
Damir Perkosan <dperkosan@gmail.com>
2022-12-20 19:14:03 +0100

    organization CRUD

--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateOrganizationDto } from './dto/create-organization.dto';
+import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { Organization } from './entities/organization.entity';
+import { OrganizationsService } from './organizations.service';
 
 @Controller('organizations')
-export class OrganizationsController {}
+export class OrganizationsController {
+  constructor(private readonly organizationsService: OrganizationsService) {}
+
+  @Post()
+  create(@Body() createOrganizationDto: CreateOrganizationDto) {
+    return this.organizationsService.create(createOrganizationDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.organizationsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: Organization['id']) {
+    return this.organizationsService.findOneOrFail({ where: { id } });
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: Organization['id'],
+    @Body() updateOrganizationDto: UpdateOrganizationDto,
+  ) {
+    return this.organizationsService.update(id, updateOrganizationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: Organization['id']) {
+    return this.organizationsService.remove(id);
+  }
+}
--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
+import { CreateOrganizationDto } from './dto/create-organization.dto';
+import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { Organization } from './entities/organization.entity';
 
 @Injectable()
-export class OrganizationsService {}
+export class OrganizationsService {
+  constructor(
+    @InjectRepository(Organization)
+    private organizationRepository: Repository<Organization>,
+  ) {}
+
+  private findWithRelations = {
+    relations: ['users'],
+  };
+
+  create(createOrganizationDto: CreateOrganizationDto) {
+    const organization = this.organizationRepository.create(
+      createOrganizationDto,
+    );
+    return this.organizationRepository.save(organization);
+  }
+
+  findAll() {
+    return this.organizationRepository.find(this.findWithRelations);
+  }
+
+  findOne(conditions: FindOneOptions<Organization>) {
+    this.organizationRepository.findOne({
+      ...conditions,
+      ...this.findWithRelations,
+    });
+  }
+
+  async findOneOrFail(conditions: FindOneOptions<Organization>) {
+    try {
+      const organization = await this.organizationRepository.findOneOrFail({
+        ...conditions,
+        ...this.findWithRelations,
+      });
+      return organization;
+    } catch (e) {
+      throw new NotFoundException('Organization not found');
+    }
+  }
+
+  async update(
+    id: Organization['id'],
+    updateOrganizationDto: UpdateOrganizationDto,
+  ) {
+    const organization = await this.organizationRepository.preload({
+      id: id,
+      ...updateOrganizationDto,
+    });
+    if (!organization) {
+      throw new NotFoundException(`Organization #${id} not found`);
+    }
+    return this.organizationRepository.save(organization);
+  }
+
+  async remove(id: Organization['id']) {
+    const organization = await this.findOneOrFail({ where: { id } });
+    return this.organizationRepository.remove(organization);
+  }
+}
