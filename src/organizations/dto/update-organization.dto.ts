80b3a0dbd1e8e7e87d8afe096c90d4a6b79c971e
Damir Perkosan <dperkosan@gmail.com>
2022-12-21 16:23:23 +0100

    dont allow users property in dto when updating organization

--- src/organizations/dto/update-organization.dto.ts
+++ src/organizations/dto/update-organization.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateOrganizationDto } from './create-organization.dto';
 
-export class UpdateOrganizationDto extends PartialType(CreateOrganizationDto) {}
+export class UpdateOrganizationDto extends PartialType(
+  OmitType(CreateOrganizationDto, ['users']),
+) {}
