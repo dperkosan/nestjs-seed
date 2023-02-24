59e3043095be07f02de6c239d3826654dc984c3a
Damir Perkosan <dperkosan@gmail.com>
2023-02-24 23:48:57 +0100

    query failed exception unit tests

--- src/common/filters/query-failed-exception/query-failed-exception.filter.spec.ts
+++ src/common/filters/query-failed-exception/query-failed-exception.filter.spec.ts
@@ -1,7 +1,55 @@
+import { ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { QueryFailedError } from 'typeorm';
+
 import { QueryFailedExceptionFilter } from './query-failed-exception.filter';
 
 describe('QueryFailedExceptionFilter', () => {
+  let filter: QueryFailedExceptionFilter<QueryFailedError>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [QueryFailedExceptionFilter<QueryFailedError>],
+    }).compile();
+
+    filter = module.get<QueryFailedExceptionFilter<QueryFailedError>>(
+      QueryFailedExceptionFilter,
+    );
+  });
+
   it('should be defined', () => {
-    expect(new QueryFailedExceptionFilter()).toBeDefined();
+    expect(filter).toBeDefined();
+  });
+
+  describe('catch', () => {
+    it('should catch and handle a QueryFailedError', () => {
+      const mockException = new QueryFailedError(
+        'SELECT * FROM users',
+        [],
+        new Error('Connection refused'),
+      );
+
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
+
+      const mockHost = {
+        switchToHttp: () => ({
+          getResponse: () => mockResponse,
+        }),
+      } as ArgumentsHost;
+
+      filter.catch(mockException, mockHost);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        errorMessage: mockException.driverError.detail,
+        errorName: mockException.name,
+      });
+    });
   });
 });
